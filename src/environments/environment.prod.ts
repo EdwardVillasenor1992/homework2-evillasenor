cd662388f61425bc8eeee75ef62d50f6b757d529
Eduardo Villasenor <eduardovillasenor26@yahoo.com>
2018-04-05 17:18:31 -0700

    fixing bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   gettyKey: 'yagbc63cvebeu4rn6xgrpbcx',
-  twitterKey: 'jNGA7fpm9zIK1qpg01LBAD6PU'
+  twitterKey: 'jNGA7fpm9zIK1qpg01LBAD6PU',
+  giphyKey: 'jFXPjBth0HY7KeO02XnC0B5bdSu2wIOJ'
+
 };
